ba9bafd917b26957ad4516ef8a635b5541fedeee
Marcus Dillavou <dillavou@vipaar.com>
2023-08-07 10:51:04 -0600

    Added a global config

---
+++ siteconfig.py
@@ -0,0 +1,26 @@
+##################################################
+# This is a global configuration for all
+#  the sample scripts. Please modify
+##################################################
+
+# CHANGE THIS TO YOUR API KEY
+# See: https://apidocs.helplightning.net/background/api-keys/
+API_KEY = 'xxxx'
+
+# CHANGE THIS TO THE LOCATION OF YOUR PRIVATE PARTNER KEY FOR YOUR WORKSPACE
+#  AND MAKE SURE YOU UPDATE THE ENTERPRISE_ID WITH YOUR WORKSPACE
+#  SITE ID
+# See: https://apidocs.helplightning.net/background/partner-keys/
+PARTNER_KEY = '/home/username/.helplightning/privatekey.pem'
+ENTERPRISE_ID = 8888 # Replace with your Site ID
+
+## US ENVIRONMENT
+HELPLIGHTNING_ENDPOINT = 'https://api.helplightning.net/api'
+HELPLIGHTNING_URL = 'https://helplightning.net'
+
+## EU ENVIRONEMNT
+# HELPLIGHTNING_ENDPOINT = 'https://api.eu1.helplightning.net/api'
+# HELPLIGHTNING_URL = 'https://app.eu1.helplightning.net'
+
+## MISC
+DEFAULT_TTL = 60 * 5
